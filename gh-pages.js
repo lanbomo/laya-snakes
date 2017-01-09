4131fa6c70f8198895703d4c2efcd4c6327db958
lanbomo <546792718@qq.com>
2017-01-09 19:02:32 +0800

    edit gh-pages

--- gh-pages.js
+++ gh-pages.js
@@ -1,7 +1,7 @@
 var ghpages = require('gh-pages');
 var path = require('path');
 
-ghpages.publish(path.join(__dirname, 'release/layaweb/1.0.0'), { push: false }, function(err) {
+ghpages.publish(path.join(__dirname, 'gh-pages'), function(err) {
     if(err){
         console.log("push error")
     }else{
